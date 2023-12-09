1e90489f264188e3d3db20b9e6fe3270b911c506
Napopegu <142897234+Napopegu@users.noreply.github.com>
2023-12-09 13:55:06 +0600

    Создал представление гостевой книги

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from webapp.models import Book
 
-# Create your views here.
+def guests_book_view(request):
+    guests_book = Book.objects.all()
+    return render(request, template_name='guests_list.html', context={'guests': guests_book})
