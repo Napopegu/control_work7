5ac196778bab04aa4e9054dbe493a03eb4e08882
Napopegu <142897234+Napopegu@users.noreply.github.com>
2023-12-09 18:16:00 +0600

    реализовал представление guess_update_view

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from webapp.views import guests_book_view,guest_add_view
+from webapp.views import guests_book_view,guest_add_view, guest_update_view
 
 urlpatterns = [
 path('',guests_book_view, name='index'),
-path('guests/add/',guest_add_view, name='guest_add_view' )
+path('guests/add/',guest_add_view, name='guest_add_view' ),
+path('guests/<int:pk>/update/',guest_update_view, name='guest_update_view' )
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from webapp.models import Book
 
 def guests_book_view(request):
@@ -14,6 +14,18 @@ def guest_add_view(request):
             author_name=request.POST.get('title'),
             author_mail=request.POST.get('mail'),
             text=request.POST.get('description')
-
         )
+        return redirect('index')
+
+def guest_update_view(request, pk):
+    guests_book = get_object_or_404(Book, pk=pk)
+    if request.method == "GET":
+        # guest = Book.objects.all()
+        return render(request, template_name='guest_update.html', context={'guests': guests_book})
+    elif request.method == "POST":
+        guests_book.author_name=request.POST.get('title')
+        guests_book.author_mail=request.POST.get('mail')
+        guests_book.text=request.POST.get('description')
+        guests_book.save()
+
         return redirect('index')
